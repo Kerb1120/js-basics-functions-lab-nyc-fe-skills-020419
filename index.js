c1029c3a7c685fc5734cf55a69b0893b5e4d5723
Kerben Bermingham <b.kerben2@gmail.com>
2019-03-12 00:32:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const FromHqInBlocks = 42
+const feetPerBlock = 264
 // Code your solution in this file!
 function distanceFromHqInBlocks(block) {
   return Math.abs(block - FromHqInBlocks)
+}
+function distanceFromHqInFeet(block){
+  return Math.abs(block - FromHqInBlocks) * feetPerBlock
 }
