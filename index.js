0c8c7c05879c0b65043ba6e433fabbb64925df63
Kerben Bermingham <b.kerben2@gmail.com>
2019-03-12 00:36:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const feetPerBlock = 264
 function distanceFromHqInBlocks(block) {
   return Math.abs(block - FromHqInBlocks)
 }
+
 function distanceFromHqInFeet(block){
   return Math.abs(block - FromHqInBlocks) * feetPerBlock
+}
+
+function distanceTravelledInFeet(block1, block2){
+  Math.abs((block1 * feetPerBlock) - (block2 * feetPerBlock))
 }
